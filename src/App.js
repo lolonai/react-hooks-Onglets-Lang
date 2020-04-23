fdd9eb7f1363ae9adf38352889698347111f83f4
laurent naitza <lolonaitza@gmail.com>
2020-04-24 01:38:09 +0200

    ajout reducer compteur exemple

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Composants/Navbar";
+import Compteur from "./Composants/Compteur";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Compteur/>
     </div>
   );
 }
